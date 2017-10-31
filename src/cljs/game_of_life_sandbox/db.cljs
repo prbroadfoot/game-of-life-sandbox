5134ea94e5496e8e934075c9e20d5c2e66a51713
Paul Broadfoot <prbroadfoot@gmail.com>
2017-10-31 10:09:05 -0500

    drag mouse to move board

--- src/cljs/game_of_life_sandbox/db.cljs
+++ src/cljs/game_of_life_sandbox/db.cljs
@@ -8,3 +8,12 @@
    :canvas-origin {:x 0 :y 0}
    :tick false
    :tick-interval 30})
+
+(defn should-shift-board? [db]
+  (let [mouse-window-coords (get-in db [:board :mouse-window-coords])
+        mouse-down-window-coords (get db :mouse-down-window-coords)]
+    (when (and (:mouse-down db)
+               (not= mouse-window-coords mouse-down-window-coords))
+      (let [delta (merge-with - mouse-window-coords mouse-down-window-coords)
+            new-origin (merge-with + (:canvas-origin db) delta)]
+        new-origin))))
--- src/cljs/game_of_life_sandbox/events.cljs
+++ src/cljs/game_of_life_sandbox/events.cljs
@@ -12,11 +12,15 @@
  :mouse-moved
  (fn  [{:keys [db]} [_ {:keys [x y]}]]
    (let [cell-size (get-in db [:board :cell-size])
-         canvas-origin (get db :canvas-origin)
-         board-x (- (quot x cell-size) (:x canvas-origin))
-         board-y (- (quot y cell-size) (:y canvas-origin))
-         mouse-down (:mouse-down db)]
-     {:db (assoc-in db [:board :mouse-coords] {:x board-x, :y board-y})})))
+         mouse-window-coords {:x (quot x cell-size), :y (quot y cell-size)}
+         mouse-coords (merge-with - mouse-window-coords (:canvas-origin db))
+         new-db (-> db
+                    (assoc-in [:board :mouse-coords] mouse-coords)
+                    (assoc-in [:board :mouse-window-coords] mouse-window-coords))]
+     (if-let [new-origin (db/should-shift-board? new-db)]
+       {:db new-db
+        :dispatch [:change-canvas-origin new-origin]}
+       {:db new-db}))))
 
 (re-frame/reg-event-fx
  :add-cell
@@ -33,8 +37,10 @@
 (re-frame/reg-event-db
  :mouse-down
  (fn [db]
-   (let [mouse-coords (get-in db [:board :mouse-coords])]
-     (assoc db :mouse-down true))))
+   (let [mouse-window-coords (get-in db [:board :mouse-window-coords])]
+     (assoc db :mouse-down true
+            :mouse-down-window-coords mouse-window-coords
+            :shift false))))
 
 (re-frame/reg-event-db
  :mouse-up
@@ -46,52 +52,56 @@
  (fn  [{:keys [db]} _]
    (let [mouse-coords (get-in db [:board :mouse-coords])
          occupied? (get-in db [:board :cells mouse-coords])]
-     (if occupied?
-       {:dispatch [:remove-cell mouse-coords]}
-       {:dispatch [:add-cell mouse-coords]}))))
+     ;; don't create a cell when clicking+dragging was used to pan the board
+     (when-not (:shift db)
+       (if occupied?
+         {:dispatch [:remove-cell mouse-coords]}
+         {:dispatch [:add-cell mouse-coords]})))))
 
 (defn in-bounds? [board cell]
-  (let [max-x (quot (:width board) (:cell-size board))
-        max-y (quot (:height board) (:cell-size board))]
-    (and (< (:x cell) max-x)
-         (< (:y cell) max-y))))
+  (let [max-x (quot (:width board) 4)
+        max-y (quot (:height board) 4)]
+    (and (< (Math/abs (:x cell)) max-x)
+         (< (Math/abs (:y cell)) max-y))))
 
 (re-frame/reg-event-fx
  :toggle-tick
  (fn [{:keys [db]} _]
-   (if (:tick db)
-     {:db (update db :tick not)}
-     {:db (update db :tick not)
-      :dispatch [:iterate-cells]} ;; what if dispatch happens before db update?
-     )))
+   (let [fx {:db (update db :tick not)}]
+     (if (:tick db)
+       fx
+       (assoc fx :dispatch [:iterate-cells])))))
 
 (re-frame/reg-event-fx
  :iterate-cells
  (fn [{:keys [db]} _]
    (let [tick (:tick db)
          board (:board db)
-         next-cells (step (get-in db [:board :cells]))]
-     (if tick
+         next-cells (->> (step (get-in db [:board :cells]))
+                         (filter (partial in-bounds? board))
+                         (into #{}))]
+     (when tick
        {:db (assoc-in db [:board :cells] next-cells)
         :draw-cells next-cells
         :dispatch-later [{:ms (:tick-interval db)
-                          :dispatch [:iterate-cells]}]}
-       {}))))
+                          :dispatch [:iterate-cells]}]}))))
 
 (re-frame/reg-event-fx
  :zoom-out
  (fn [{:keys [db]} _]
-   {:db (update-in db [:board :cell-size] #(max (quot % 1.5) 1))
+   {:db (update-in db [:board :cell-size] #(max (quot % 1.2) 2))
     :draw-cells (get-in db [:board :cells])}))
 
 (re-frame/reg-event-fx
  :zoom-in
  (fn [{:keys [db]} _]
-   {:db (update-in db [:board :cell-size] #(Math/ceil (* % 1.5)))
+   {:db (update-in db [:board :cell-size] #(Math/ceil (* % 1.2)))
     :draw-cells (get-in db [:board :cells])}))
 
 (re-frame/reg-event-fx
  :change-canvas-origin
  (fn [{:keys [db]} [_ new-origin]]
-   {:db (assoc db :canvas-origin new-origin)
+   {:db (assoc db :canvas-origin new-origin
+               :mouse-down-window-coords (get-in db [:board :mouse-window-coords])
+               :shift true)
     :draw-cells (get-in db [:board :cells])}))
